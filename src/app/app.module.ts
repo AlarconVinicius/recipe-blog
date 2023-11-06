3ca4f92f8edcabc27fb6533b6c90e933ab38f095
AlarconVinicius <alarcon.pqd74@gmail.com>
2023-11-06 17:40:36 -0300

    feat: Add login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -52,7 +53,8 @@ import { RegisterComponent } from './pages/auth-pages/register/register.componen
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/auth-pages/login/login.component.ts
+++ src/app/pages/auth-pages/login/login.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Login } from 'src/app/models/auth/login';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginObj: Login = {
+    email: '',
+    password: ''
+  };
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
   }
 
+  onLogin() {
+    // debugger;
+    this.http.post('https://localhost:7251/api/auth/login', this.loginObj).subscribe(
+      (res: any) => {
+        if (res.success != false) {
+          console.log('Login OK');
+        }
+      },
+      (error) => {
+        if(error.status == 400){
+          console.log('Erro na solicitação HTTP: ', error.error);
+        } else {
+          console.log('Erro desconhecido');
+        }
+      }
+    );
+  }
 }
